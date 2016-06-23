f7365b4af8c916a9452a0f843de5fb1dffb4934e
Sebastian Fuchs <sfuchs@projektkater.de>
2016-06-23 19:15:09 +0200

    add millerntor gallery theme

---
+++ README.md
@@ -0,0 +1,15 @@
+# theme_millerntor
+
+Twitter Bootstrap theme for TYPO3 CMS. Based on theme_bootstrap
+
+![Screenshot](Meta/Screenshots/screenshot.png)
+
+Slate is a very basic theme. It can be used to start your own theme.
+
+It is used by the themes Team mainly for testing and experimenting with new functionalities.
+
+<!---
+![Screenshot](Meta/Screenshots/screenshot-01.png)
+![Screenshot](Meta/Screenshots/screenshot-02.png)
+![Screenshot](Meta/Screenshots/screenshot-03.png)
+-->
---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "templavoila_framework".
+#
+# Auto generated 25-08-2011 21:37
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Millerntor Gallery Theme' ,
+	'description' => 'Theme to use Twitter Bootstrap',
+	'category' => 'templates',
+	'shy' => 0,
+	'version' => '7.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Sebastian Fuchs',
+	'author_email' => 'sfuchs@projektkater.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3'  => '7.6.0-7.6.99',
+			'theme_bootstrap' => '7.0.0-7.99.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+);
+
+?>
